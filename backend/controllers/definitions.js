4a8ebd4e89094d1dc92dad362d6b7dd3d6bfcd51
Sarah Berri <sarahberri@outlook.fr>
2023-04-30 13:42:14 +0200

    complete

--- backend/controllers/definitions.js
+++ backend/controllers/definitions.js
@@ -1,30 +1,63 @@
 const Definition = require('../models/definitions');
 
-exports.createDefinition =(req, res) =>{
-    const def = new Definition(req.body);
-    def.save()
-    .then((definition) =>{
-        return res.status(201).json({definition})
-    })
-    .catch((error)=> {
-        return res.status(400).json({error})
-    });
+exports.createDefinition = (req, res) => {
+    const { word, definition } = req.body;
+    const newDefinition = new Definition({ word, definition });
+
+    console.log('Creating new definition:', newDefinition);
+
+    newDefinition.save()
+        .then((definition) => {
+            console.log('New definition created:', definition);
+            return res.status(201).json({
+                message: "Definition created successfully",
+                definition: { _id: definition._id, word: definition.word, definition: definition.definition }
+            });
+        })
+        .catch((error) => {
+            console.error('Error creating definition:', error);
+            return res.status(400).json({ error });
+        });
 };
 
 exports.getOneDefinition = (req, res) => {
     const id = req.params.id;
 
-    Definition.findOne({_id:id})
-        .then((definition) =>{
-            return res.status(200).json({definition})})
-            .catch((error) =>{
-                return res.status(400).json({error})});
+    console.log('Getting definition with id:', id);
+
+    Definition.findOne({ _id: id })
+        .then((definition) => {
+            if (!definition) {
+                console.error('Definition not found with id:', id);
+                return res.status(404).json({ error: "Definition not found" });
+            }
+            console.log('Definition found:', definition);
+            return res.status(200).json({
+                _id: definition._id,
+                word: definition.word,
+                definition: definition.definition
+            });
+        })
+        .catch((error) => {
+            console.error('Error getting definition with id:', id, error);
+            return res.status(400).json({ error });
+        });
 };
 
+
 exports.getAllDefinitions = (req, res) => {
+    console.log('Getting all definitions');
+
     Definition.find()
-        .then((definitions) =>{
-            return res.status(200).json({definitions})})
-            .catch((error) =>{
-                return res.status(400).json({error})});
+        .then((definitions) => {
+            const definitionsList = definitions.map((definition) => {
+                return { _id: definition._id, word: definition.word, definition: definition.definition };
+            });
+            console.log('Definitions found:', definitionsList);
+            return res.status(200).json({ definitions: definitionsList });
+        })
+        .catch((error) => {
+            console.error('Error getting all definitions:', error);
+            return res.status(400).json({ error });
+        });
 };
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,22 +1,24 @@
-function search(){
-  let word = document.getElementById("search-term").value;
-  let resultat = document.getElementById("resultat");
-
-  if(word.length != 0) {
-
-    fetch("localhost:3000/api/definitions/" + word)
-    .then(res =>
-        res.json().then(data =>{
-            try{
-                for(let words of data){
-                    let definition = words.meanings[0].definitions[0].definition
-                    resultat.innerHTML="<p class='res'><span>Definition</span><br>"+ definition+"</p>";
-                }
-            }catch(err){
-                resultat.innerHTML="<p>Sorry...No definition was found!</p>"
-            
-            }
-        }))
-            }else{
-                resultat.innerHTML = "<p style='color:red;'>* This field must be filled</p>"    }
-}
+function search() {
+    let word = document.getElementById("search-term").value;
+    let resultat = document.getElementById("resultat");
+  
+    if (word.length != 0) {
+      fetch("http://localhost:3000/api/definitions/?search=" + word)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data); // afficher la réponse dans la console
+          try {
+            let definition = data.definitions.filter(def => def.word.toLowerCase() === word.toLowerCase())[0].definition;
+            resultat.innerHTML = "<p class='res'><span>Definition</span><br>" + definition + "</p>";
+          } catch(err) {
+            resultat.innerHTML = "<p>Sorry...No definition was found!</p>";
+          }
+        })
+        .catch(err => {
+          resultat.innerHTML = "<p>Sorry...An error occured while fetching data!</p>";
+        });
+    } else {
+      resultat.innerHTML = "<p style='color:red;'>* This field must be filled</p>";
+    }
+  }
+  
